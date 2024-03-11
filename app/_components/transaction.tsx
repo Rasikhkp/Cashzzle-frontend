2dd9a9ebdfaec056aa6bf789811de5f1bc5acc26
rasikhkp <raxiest@gmail.com>
2024-03-11 09:28:37 +0700

    feat: change client side edit and delete transaction style

--- app/_components/transaction.tsx
+++ app/_components/transaction.tsx
@@ -3,19 +3,21 @@
 import { Button } from "@/components/ui/button";
 import {
     EllipsisVerticalIcon,
+    PencilIcon,
     PencilSquareIcon,
     TrashIcon,
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import food from "@/public/food.svg";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Transaction = () => {
     const [openOption, setOpenOption] = useState(false);
 
     return (
-        <div className="flex gap-3 items-center relative">
-            <div className="w-full flex items-center justify-between rounded-xl p-3 bg-white border border-gray-500">
+        <div className="flex items-center relative">
+            <div className="w-full mr-3 flex items-center justify-between rounded-xl p-3 bg-white border border-gray-500">
                 <div className="flex gap-3">
                     <Image alt="food" src={food} width={40} />
                     <div className="flex flex-col justify-center">
@@ -27,26 +29,49 @@ const Transaction = () => {
                 <div className="font-bold text-red-500 text-sm">Rp30.000</div>
             </div>
 
-            <Button
+            <AnimatePresence>
+                {openOption && (
+                    <motion.div
+                        className="flex gap-3 overflow-clip"
+                        initial={{
+                            width: 0
+                        }}
+                        animate={{
+                            width: 120,
+                            transition: {
+                                type: "spring",
+                                bounce: .3,
+                                duration: .4
+                            }
+                        }}
+                        exit={{
+                            width: 0,
+                            gap: 0,
+                            transition: {
+                                type: "easeIn",
+                                duration: .2
+                            }
+                        }}
+                    >
+                        <button className="transition-all rounded-full w-8 h-8 hover:bg-gray-100 active:bg-gray-200 flex justify-center items-center">
+                            <TrashIcon className="w-4" />
+                        </button>
+
+                        <button className="transition-all rounded-full w-8 h-8 hover:bg-gray-100 active:bg-gray-200 flex justify-center items-center">
+                            <PencilIcon className="w-4" />
+                        </button>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
+
+            <button
+                className="w-8 h-8 mx-2 flex flex-none justify-center items-center rounded-full hover:bg-gray-100 active:bg-gray-200 transition-all"
                 onClick={() => setOpenOption(!openOption)}
-                variant={"ghost"}
-                size={"icon"}
             >
                 <EllipsisVerticalIcon className="h-6" />
-            </Button>
+            </button>
 
-            <div
-                className={`${
-                    openOption ? "opacity-100 z-10" : "opacity-0 -z-0"
-                } transition-all duration-300 rounded-xl options flex flex-col bg-white p-1 border border-gray-500 absolute gap-1 -bottom-14 right-0`}
-            >
-                <button className="rounded-lg flex items-center justify-center w-6 h-6 transition-all hover:bg-gray-200 active:bg-gray-300">
-                    <PencilSquareIcon className="w-4" />
-                </button>
-                <button className="rounded-lg flex items-center justify-center w-6 h-6 transition-all hover:bg-gray-200 active:bg-gray-300">
-                    <TrashIcon className="w-4 text-red-500" />
-                </button>
-            </div>
         </div>
     );
 };
